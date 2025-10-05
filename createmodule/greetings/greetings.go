c22d862c5687efc69944cb379046a9f344f14a23
devpedropp <pedroppimentel3@gmail.com>
2025-10-05 11:25:43 -0300

    feat: add error handling

--- createmodule/greetings/greetings.go
+++ createmodule/greetings/greetings.go
@@ -1,16 +1,28 @@
 /*
 * https://go.dev/doc/tutorial/create-module
+* https://go.dev/doc/tutorial/handle-errors
 **/
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 * A function whose name starts with a capital letter
 * can be called by a function not in the same package.
 * This is known in Go as an exported name. (https://go.dev/tour/basics/3)
+*
+* Any Go function can return multiple values. (https://go.dev/doc/effective_go#multiple-returns)
 **/
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+
+	// nil, means "no error"
+	return message, nil
 }
--- createmodule/hello/hello.go
+++ createmodule/hello/hello.go
@@ -5,11 +5,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// an empty name will return error
+	message, err := greetings.Hello("")
+	// a name will return the greeting
+	// message, err := greetings.Hello("Gladys")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
